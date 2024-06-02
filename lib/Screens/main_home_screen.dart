40ce683a0b6828bc0595d58e5be4185442bf3977
Meghanathan-P-H <mnsocialmediaoffl@gmail.com>
2024-06-02 16:29:00 +0530

    navigation bar setting

--- lib/Screens/main_home_screen.dart
+++ lib/Screens/main_home_screen.dart
@@ -8,20 +8,52 @@ class ScreenHome extends StatefulWidget {
 }
 
 class _ScreenHomeState extends State<ScreenHome> {
+  int _selectIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: Transform.translate( offset:const Offset(15, 0),
-          child:const CircleAvatar(
+        leading: Transform.translate(
+          offset: const Offset(15, 0),
+          child: const CircleAvatar(
             backgroundColor: Colors.transparent,
-            backgroundImage: AssetImage('asset/images/MelodiaLogo.png'),radius: 50,
+            backgroundImage: AssetImage('asset/images/MelodiaLogo.png'),
+            radius: 50,
           ),
         ),
-        actions: [IconButton(onPressed: (){}, icon:const Icon(Icons.settings_suggest_outlined,color: Colors.white,size: 45,))],
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.settings_suggest_outlined,
+                color: Colors.white,
+                size: 45,
+              ))
+        ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFF28282F),
+          type: BottomNavigationBarType.fixed,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.white,
+          currentIndex: _selectIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectIndex = index;
+            });
+          },iconSize: 30,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_rounded), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search_rounded), label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border_rounded), label: 'Favorite'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.my_library_music_outlined), label: 'Playlist')
+          ]),
     );
   }
 }
