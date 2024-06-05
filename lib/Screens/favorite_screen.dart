72151b31e0d9d65d89288e139b3d8738595b53d3
Meghanathan-P-H <mnsocialmediaoffl@gmail.com>
2024-06-05 13:07:23 +0530

    two page basic design completed

--- lib/Screens/favorite_screen.dart
+++ lib/Screens/favorite_screen.dart
@@ -10,6 +10,47 @@ class ScreenFovorite extends StatefulWidget {
 class _ScreenFovoriteState extends State<ScreenFovorite> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Favorite Screen'),),);
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF7D7D7D), Color(0xED262626)])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            AppBar(
+              backgroundColor: Colors.transparent,
+              title: const Text('Favorite Songs',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26)),
+              centerTitle: true,
+            ),
+            const SizedBox(height: 15,),
+            Padding(
+              padding: const EdgeInsets.only(left: 16,right: 16),
+              child: Container(
+                height: 60,
+                padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 8.0),
+                decoration: BoxDecoration(
+                    color: Colors.white, borderRadius: BorderRadius.circular(30)),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search here',
+                      hintStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),border: InputBorder.none),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Screens/search_screen.dart
+++ lib/Screens/search_screen.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
 
-
 class ScreenSearch extends StatelessWidget {
-  const ScreenSearch ({super.key});
+  const ScreenSearch({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.width,
-      decoration:const BoxDecoration(
-          gradient: LinearGradient(colors: [Color(0xFF7D7D7D), Color(0xFF171717)])),
-      child:const SafeArea(child: Center(child: Text('data'),)),
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF7D7D7D), Color(0xED262626)])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              title: const Text(
+                'Search Songs',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    image: AssetImage('asset/images/serchscreenimagebc.png'),
+                    fit: BoxFit.cover,
+                  )),
+                ),
+                Positioned(
+                  left: 16,
+                  right: 16,
+                  bottom: 1,
+                  child: Container(
+                    height: 60,
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Search here',
+                          hintStyle: TextStyle(color: Colors.black),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                          ),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
