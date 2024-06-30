120044ad33cdb0c3014f4a0837867318b517bad7
Meghanathan-P-H <mnsocialmediaoffl@gmail.com>
2024-06-30 09:23:17 +0530

    ckd

--- lib/widgets/reusing_widgets.dart
+++ lib/widgets/reusing_widgets.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 backgroundTheme() {
   return const LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Color(0xFF7D7D7D), Color(0xED262626)]);
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [Color(0xFF7D7D7D), Color(0xED262626)]);
+}
+
+widgetbackgroundTm() {
+  return const LinearGradient(
+      begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color(0xFF666666), Color(0xB8000000)]);
 }
