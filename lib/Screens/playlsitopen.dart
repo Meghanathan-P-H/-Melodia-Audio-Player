7ed5381b34527b0a95720554022a7111d7a04daf
Meghanathan-P-H <mnsocialmediaoffl@gmail.com>
2024-07-01 10:04:45 +0530

    playlist done

--- lib/Screens/playlsitopen.dart
+++ lib/Screens/playlsitopen.dart
@@ -4,7 +4,6 @@ import 'package:melodia_audioplayer/db_model/db_model.dart';
 import 'package:melodia_audioplayer/db_model/db_playlistmodel.dart';
 import 'package:melodia_audioplayer/screens/allsongplaylist.dart';
 import 'package:melodia_audioplayer/screens/musicplay_screen.dart';
-import 'package:melodia_audioplayer/screens/permission_provider.dart';
 import 'package:melodia_audioplayer/widgets/reusing_widgets.dart';
 
 // ignore: must_be_immutable
@@ -20,121 +19,134 @@ class OpenPlayList extends StatefulWidget {
 
 class _OpenPlayListState extends State<OpenPlayList> {
   TextEditingController textFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(gradient: backgroundTheme()),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Column(
-          children: [
-            AppBar(
-              iconTheme: const IconThemeData(color: Colors.white),
-              backgroundColor: const Color(0xFF282C28),
-              title: Text(
-                widget.playlistname,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              centerTitle: true,
-              actions: [
-                Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => AllSongsPlaylist(playlistModel: widget.playListmodel,)))
-                                .then((value) {
-                              setState(() {});
-                            });
-                        },
-                        icon: const Icon(
-                          Icons.add_circle_outline_rounded,
-                          color: Colors.white,
-                          size: 30,
-                        ))),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Expanded(
-                child: FutureBuilder(
-                    future: playlistSongs(playlist: widget.playListmodel),
-                    builder: (context, item) {
-                      if (item.data == null) {
-                        SongFetcher();
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (item.data!.isEmpty) {
-                        return const Center(
-                            child: Text('No songs found',
-                                style: TextStyle(color: Colors.white)));
-                      } else if (item.connectionState ==
-                          ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (item.hasError) {
-                        return const Center(
-                          child: Text('Error No Songs Available'),
-                        );
-                      } else {
-                        return ListView.builder(
-                          padding: EdgeInsets.zero,
-                          itemCount: item.data!.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                                contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0,
-                                  vertical: 8.0,
-                                ),
-                                leading: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: Image.asset(
-                                    'asset/images/musicimage.png',
-                                    width: 50,
-                                    height: 50,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                title: Text(
-                                  item.data![index].name,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                subtitle: Text(
-                                  item.data![index].artist,
-                                  style: const TextStyle(color: Colors.white70),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                trailing: IconButton(
-                                    onPressed: () {
-                                      removeSongsFromplaylsit(
-                                          songid: item.data![index].musicid,
-                                          playlist: widget.playListmodel);
-                                          setState(() {});
-                                    },
-                                    icon: const Icon(
-                                      Icons.delete_rounded,
-                                      color: Colors.white,
-                                    )),
-                                onTap: () {
-                                  final song = item.data![index];
-                                  _navigateToMusicPlayScreen(context, song);
-                                });
-                          },
-                        );
-                      }
-                    }))
-          ],
+        appBar: _buildAppBar(),
+        body: _buildBody(),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      iconTheme: const IconThemeData(color: Colors.white),
+      backgroundColor: const Color(0xFF282C28),
+      title: Text(
+        widget.playlistname,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 23,
+          fontWeight: FontWeight.bold,
         ),
       ),
+      centerTitle: true,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AllSongsPlaylist(
+                    playlistModel: widget.playListmodel,
+                  ),
+                ),
+              ).then((value) {
+                setState(() {});
+              });
+            },
+            icon: const Icon(
+              Icons.add_circle_outline_rounded,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        Expanded(
+          child: FutureBuilder<List<SongMusic>>(
+            future: playlistSongs(playlist: widget.playListmodel),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return const Center(child: Text('Error No Songs Available'));
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(
+                  child: Text(
+                    'No songs found',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return _buildSongTile(snapshot.data![index]);
+                  },
+                );
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSongTile(SongMusic song) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 8.0,
+      ),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.asset(
+          'asset/images/musicimage.png',
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: Text(
+        song.name,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        overflow: TextOverflow.ellipsis,
+      ),
+      subtitle: Text(
+        song.artist,
+        style: const TextStyle(color: Colors.white70),
+        overflow: TextOverflow.ellipsis,
+      ),
+      trailing: IconButton(
+        onPressed: () {
+          removeSongsFromplaylsit(
+              songid: song.musicid, playlist: widget.playListmodel);
+          setState(() {});
+        },
+        icon: const Icon(
+          Icons.delete_rounded,
+          color: Colors.white,
+        ),
+      ),
+      onTap: () => _navigateToMusicPlayScreen(context, song),
     );
   }
 
